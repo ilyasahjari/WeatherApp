b55c4f62d5e598845344cf4f2b138b18ab2e8422
ilyas <56867392+ilyasahjari@users.noreply.github.com>
2020-09-21 18:31:27 +0200

    added port in app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 const app = express();
 const templatesPath = path.join(__dirname, "../views/templates")
 const partialsPath = path.join(__dirname,"../views/partials")
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath))
 app.set('view engine', 'hbs');
